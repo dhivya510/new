bf6f5255c1e5809546c222cff1ea9eba58e8b792
dhivya510 <67903535+dhivya510@users.noreply.github.com>
2020-07-09 14:38:08 +0530

    Update example.java

--- example.java
+++ example.java
@@ -1,11 +1,5 @@
-import java.util.Calendar;
-
-public class Exercise15 {
- 
- public static void main(String[] args) { 
-    Calendar c = Calendar.getInstance();
-      System.out.println("Current Date and Time :"); 
-      System.out.format("%tB %te, %tY%n", c, c, c);
-      System.out.format("%tl:%tM %tp%n", c, c, c); 
-   }
-   }
+public class Hello {
+    public static void main(String args[]){
+        System.out.println("I'm dancing");
+    }
+}
