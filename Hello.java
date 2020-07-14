39bfa26b3e55afdae34f831be77cba4106add36a
dhivya510 <67903535+dhivya510@users.noreply.github.com>
2020-07-14 12:04:37 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
-public class Hello {
-    public static void main(String args[]){
-        System.out.println("I'm dancing");
-    }
+import java.util.*; 
+public class Hello { 
+    public static void main(String args[]) 
+    { 
+        Calendar c = Calendar.getInstance(); 
+        System.out.println("The Current Date is:" + c.getTime()); 
+    } 
 }
